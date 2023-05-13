96f357da9fd0fa5f8b15f63d4ea523049960b40d
kausar perveen <chandamughal12@gmail.com>
2023-05-13 15:37:57 +0500

    modified code

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
-  
+  require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -17,54 +17,55 @@ const http = require('http');
 const path = require('path');
 
 /**
- * Get port from environment and store in Express.
- */
+* Get port from environment and store in Express.
+*/
 
 const port = normalizePort(process.env.PORT || '3000');
 
 /**
- * Normalize a port into a number, string, or false.
- */
+* Normalize a port into a number, string, or false.
+*/
 function normalizePort(val) {
   const port = parseInt(val, 10);
   if (isNaN(port)) {
-    // named pipe
-    return val;
+      // named pipe
+      return val;
   }
   if (port >= 0) {
-    // port number
-    return port;
+      // port number
+      return port;
   }
   return false;
 }
 
 /**
- * Create Express app.
- */
+* Create Express app.
+*/
 const app = express();
 
 /**
- * Set up middleware.
- */
+* Set up middleware.
+*/
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+      // Retrieve data from req.body
+      const postData = req.body;
+
+      // Perform necessary operations with postData
+      console.log('POST request intercepted with data:', postData);
+  }
+
+  next();
+});
 app.use('/users', userRoutes);
 app.use('/', indexRoutes);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.post('*', (req, res, next) => {
-  // Retrieve data from req.body
-  const postData = req.body;
-
-  // Perform necessary operations with postData
-  console.log('POST request intercepted with data:', postData);
-
-  next();
-});
-
 
 /**
- * Connect to MongoDB.
- */
+* Connect to MongoDB.
+*/
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -80,34 +81,34 @@ mongoose.connect(process.env.DB_URI, {
    * Event listener for HTTP server "error" event.
    */
   function onError(error) {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
+      if (error.syscall !== 'listen') {
+          throw error;
+      }
 
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+      const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
+      // handle specific listen errors with friendly messages
+      switch (error.code) {
+          case 'EACCES':
+              console.error(bind + ' requires elevated privileges');
+              process.exit(1);
+              break;
+          case 'EADDRINUSE':
+              console.error(bind + ' is already in use');
+              process.exit(1);
+              break;
+          default:
+              throw error;
+      }
   }
 
   /**
    * Event listener for HTTP server "listening" event.
    */
   function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+      const addr = server.address();
+      const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+      debug('Listening on ' + bind);
   }
 
   /**
