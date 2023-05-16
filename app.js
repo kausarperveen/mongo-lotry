e99d64a8166e07d093c72d0205cb3d06b1dac317
kousar <kousar@jawads-MacBook-Pro-2.local>
2023-05-16 17:48:59 +0500

    2nd

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ const port = normalizePort(process.env.PORT || '3000');
 function normalizePort(val) {
   const port = parseInt(val, 10);
   if (isNaN(port)) {
-      // named pipe
-      return val;
+    // named pipe
+    return val;
   }
   if (port >= 0) {
-      // port number
-      return port;
+    // port number
+    return port;
   }
   return false;
 }
@@ -43,8 +43,19 @@ function normalizePort(val) {
 */
 const app = express();
 
+// Middleware to handle all POST requests
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+    // Handle the POST request here
+    console.log('Received a POST request');
+    // Perform necessary actions
 
-//Middleware to handle all POST requests
+    // Return a response
+    res.send('POST request processed successfully');
+  } else {
+    next(); // Pass the request to the next middleware/route handler
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -57,60 +68,63 @@ app.set('views', path.join(__dirname, 'views'));
 /**
 * Connect to MongoDB.
 */
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('db connected');
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('db connected');
 
-  /**
-   * Create HTTP server.
-   */
-  const server = http.createServer(app);
+    /**
+     * Create HTTP server.
+     */
+    const server = http.createServer(app);
 
-  /**
-   * Event listener for HTTP server "error" event.
-   */
-  function onError(error) {
+    /**
+     * Event listener for HTTP server "error" event.
+     */
+    function onError(error) {
       if (error.syscall !== 'listen') {
-          throw error;
+        throw error;
       }
 
       const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
       // handle specific listen errors with friendly messages
       switch (error.code) {
-          case 'EACCES':
-              console.error(bind + ' requires elevated privileges');
-              process.exit(1);
-              break;
-          case 'EADDRINUSE':
-              console.error(bind + ' is already in use');
-              process.exit(1);
-              break;
-          default:
-              throw error;
+        case 'EACCES':
+          console.error(bind + ' requires elevated privileges');
+          process.exit(1);
+          break;
+        case 'EADDRINUSE':
+          console.error(bind + ' is already in use');
+          process.exit(1);
+          break;
+        default:
+          throw error;
       }
-  }
+    }
 
-  /**
-   * Event listener for HTTP server "listening" event.
-   */
-  function onListening() {
+    /**
+     * Event listener for HTTP server "listening" event.
+     */
+    function onListening() {
       const addr = server.address();
       const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
       debug('Listening on ' + bind);
-  }
+    }
 
-  /**
-   * Listen on provided port, on all network interfaces.
-   */
-  server.listen(port);
-  server.on('error', onError);
-  server.on('listening', onListening);
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
+    server.listen(port);
+    server.on('error', onError);
+    server.on('listening', onListening);
 
-  console.log(`Server listening on port ${port}`);
-}).catch((error) => {
-  console.log(error);
-  process.exit(1);
-});
+    console.log(`Server listening on port ${port}`);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
